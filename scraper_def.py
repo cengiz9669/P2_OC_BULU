60b7b0ea23886feb789feef6bf6ea1d86511b86c
cengiz96 <oblimontepe@hotmail.fr>
2022-02-27 13:59:51 +0100

    Programme def

--- scraper_def.py
+++ scraper_def.py
@@ -105,3 +105,139 @@ def telechargement_image_livre(image_url_value: str, upc_value: str):
     image_path = repertoire_de_travail + '/Donnees_Resultat' + '/Couvertures' + '/' + upc_value + '.jpeg'
     open(image_path, 'wb').write(r.content)
 
+
+def ecriture_csv(categorie_livre: str, liste_donnees_par_livre: List[Dict[str, str]]):
+    '''Fonction pour créer le fichier CSV pour une catégorie donnée.
+    Le fichier portera le nom de la catégorie entrée en argument.'''
+
+    headers_csv = [
+        'product_page_url',
+        'universal_product_code',
+        'title',
+        'price_including_tax',
+        'price_excluding_tax',
+        'number_available',
+        'product_description',
+        'category',
+        'review_rating',
+        'image_url',
+    ]
+
+    # Création Dossier 'Donnees_Resultat' si nécessaire dans le répertoire de travail
+    if os.path.exists(repertoire_de_travail + '/Donnees_Resultat') == False:
+        os.makedirs(repertoire_de_travail + '/Donnees_Resultat')
+
+    # Création du CSV portant le nom de la catégorie et écriture des entêtes
+    nom_du_csv = repertoire_de_travail + '/Donnees_Resultat/' + str(categorie_livre) + '.csv'
+    print(f'Ecriture du csv {nom_du_csv}')
+
+    with open(nom_du_csv, 'w', newline='', encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers_csv)
+        writer.writeheader()
+        writer.writerows(liste_donnees_par_livre)
+
+    print(f'{len(liste_donnees_par_livre)} livres écrits pour la catégorie {categorie_livre}.')
+
+
+def extraire_liste_livres(categorie_livre_url :str) -> List[str]:
+    '''Fonction permettant d'obtenir en sortie une liste contenant l'url de chacun des livres présents dans la catégorie.
+    Cette fonction est récursive pour tenir compte de la pagination.'''
+
+    # Initialisation requête
+    response = requests.get(categorie_livre_url)
+
+    # Récupération des informations
+    if response.ok:
+
+        # HTML PARSER
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Recherche des url de livres
+        blocks_livres = soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
+        donnees_liste_livre = []
+        for livre in blocks_livres:
+            donnees_liste_livre.append(str('http://books.toscrape.com/catalogue/'+ str(livre).split('"')[7].split('../../../')[1]))
+
+        # Detection page 'Next' et itération
+        block_url_page_suivante = soup.find_all('li', class_='next')
+        if block_url_page_suivante != []:
+            url_livre_base = ''
+            url_livre_base_liste = categorie_livre_url.split('/')[:-1]
+            for element in url_livre_base_liste:
+                url_livre_base += str(element) + '/'
+            donnees_liste_livre += extraire_liste_livres(
+                url_livre_base + str(block_url_page_suivante[0]).split('"')[3]
+            )
+
+        return donnees_liste_livre
+
+
+def ecriture_categorie(categorie_livre_url :str):
+    '''Function permettant d'extraire toutes les informations de tous les livres d'un même catégorie.
+    Toutes ces informations seront écrites sur un même fichier CSV portant le nom de la categorie'''
+
+    # Extraire liste des url de livres
+    liste_livre = extraire_liste_livres(categorie_livre_url)
+
+    # Inititalisation CSV par catégorie
+    categorie: str = recuperation_informations_page_livre(liste_livre[0])['category']
+
+    # Itération sur tous les livres dans la catégorie
+    donnees_par_livre = []
+    for livre in liste_livre:
+        donnees_par_livre.append(recuperation_informations_page_livre(livre))
+
+    ecriture_csv(categorie, donnees_par_livre)
+
+
+def extraire_list_categorie_url_extraction(contenu_html: str) -> List[str]:
+    # HTML PARSER
+    soup = BeautifulSoup(contenu_html, 'html.parser')
+    liste_categorie = []
+
+    # Block Liste de categorie
+    block_categories = soup.find('ul', class_='nav nav-list')
+    block_categories_lignes = block_categories.find_all('a')
+
+    # Itération sur chaque categorie
+    for categorie in range(1, len(block_categories_lignes)):
+        liste_categorie.append(
+            str(
+                'https://books.toscrape.com/catalogue/category/'
+                + str(block_categories_lignes[categorie])
+                .split('"')[1]
+                .split('../')[1]
+            )
+        )
+    return liste_categorie
+    
+
+def extraire_liste_categorie_url() -> List[str]:
+    '''Function permettant d'extraire dans une liste l'ensemble des categories disponibles sous forme d'url.'''
+
+    # Initialisation requête + liste_categorie
+    response = requests.get(books_home_url)
+    # Récupération des informations
+    if response.ok:
+        categories = extraire_list_categorie_url_extraction(response.text)
+        return categories
+
+
+def extraire_tout():
+    '''Fonction permettant d'extraire les informations de tous les produits parmis toutes les catégories du site'''
+
+    # Extraction de la liste des catégories
+    print(f"Début de l'extraction des catégories")
+    liste_categorie = extraire_liste_categorie_url()
+    print(f"{len(liste_categorie)} catégories extraites")
+    print('------ Traitement par catégorie ------')
+
+    # Extraction des informations de tous les livres pour chaque catégorie
+    for categorie_url in liste_categorie:
+        print(f"Traitement de la catégorie {categorie_url}")
+        ecriture_categorie(categorie_url)
+        print('------------')
+
+
+if __name__ == '__main__':
+    extraire_tout()
